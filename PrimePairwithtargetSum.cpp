24795d8d97bd25ea488272734b65cd67e6b2d9bb
Arya Putatunda <136840478+Movazed@users.noreply.github.com>
2024-06-16 23:37:18 +0530

    Add files via upload

---
+++ PrimePairwithtargetSum.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Solution {
+  public:
+    vector<int> getPrimes(int n) {
+        vector<bool> is_prime = sieve(n);
+        for (int i = 2; i <= n / 2; ++i) {
+            if (is_prime[i] && is_prime[n - i]) {
+                return {i, n - i};
+            }
+        }
+        return {-1, -1};
+    }
+
+private:
+    vector<bool> sieve(int n) {
+        vector<bool> is_prime(n + 1, true);
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i * i <= n; ++i) {
+            if (is_prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    is_prime[j] = false;
+                }
+            }
+        }
+        return is_prime;
+    }
+};
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        vector<int> res = obj.getPrimes(n);
+
+        Array::print(res);
+    }
+}
